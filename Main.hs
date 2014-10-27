ab63e893deea97ec29648e5d033eff016d109c9a
Alexandre Medeiros <alexandre.n.medeiros@gmail.com>
2014-10-27 21:43:53 +0000

    Add musicbrainz testing to Main.hs
    The resulting binary now expects a list of file names as its arguments and reads
    and parses all these files.

--- Main.hs
+++ Main.hs
@@ -5,11 +5,34 @@
 --
 -- Main file for ECS713 Functional Programming Project
 
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import AesonSample
+--import AesonSample
+import Datatypes
 
+import Data.Aeson
+import Data.ByteString.Lazy
 import Data.Maybe
 
+import System.Environment
+
+{-
 main :: IO ()
 main = print $ fromJust aesonSample
+-}
+
+readArtist :: ByteString -> Maybe Artist
+readArtist = decode
+
+main :: IO ()
+main = do
+        fnames <- getArgs
+        let dataset = Prelude.mapM parseFile fnames
+        print dataset
+
+parseFile :: String -> Maybe Artist
+parseFile fn = do
+        contents <- Data.ByteString.Lazy.readFile fn
+        decode contents
