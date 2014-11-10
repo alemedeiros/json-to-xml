dd50b346902b5b788da25035995417bd8a14fd9f
Alexandre Medeiros <alexandre.n.medeiros@gmail.com>
2014-11-10 11:03:45 +0000

    Fix and clean up files

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 -- Main.hs
---  by alemedeiros <alexandre.n.medeiros _at_ gmail.com>
+--  by Alexandre Medeiros <alexandre.n.medeiros _at_ gmail.com>
+--     Tom Hedges <t.w.hedges _at_ qmul.ac.uk>
 --
 -- JSON to XML translation using Aeson
 --
@@ -10,140 +11,92 @@
 module Main where
 
 import Datatypes
+import Queries
 import ToXml
-import System.FilePath
-import Text.XML.Light
 
 import Control.Monad
 
 import Data.Aeson
 import qualified Data.ByteString.Lazy as BS
-import Data.List
 import Data.Maybe
 
+import System.Directory
+import System.Environment
+import System.FilePath
 import System.IO
 
-import System.Environment
-import System.Directory
+import Text.XML.Light
 
--- This functions gets the string contents of a json file and returns an artist
-readArtist :: BS.ByteString -> Artist
-readArtist = fromMaybe NullArtist . decode
-
+-- Main function
 main :: IO ()
 main = do
+        -- Get file names from arguments
         fileNames <- getArgs
-        dataStr <- mapM BS.readFile fileNames
-        let
-            artistData = map readArtist dataStr
-        putStrLn "Showing read data"
-        print artistData
 
-       --convert to xml and put in file
-        let xmlData = map makeXmlArtist artistData
-        putStrLn "Showing XML"
-        print xmlData
-        zipWithM writeToFile xmlData (map generateFilePath fileNames)
-        
-        doQueries artistData
+        runFromFileList fileNames
 
-        putStrLn "End"
+{--- Run parsing functions ---}
 
--- This works, main doesn't. No idea why
--- error is json-to-xml: No match in record selector artistAliases
--- after line putStrLn xmlData
-        
+-- Run parsing and conversion on a list of files
 runFromFileList :: [FilePath] -> IO ()
 runFromFileList filePaths = do
-
+        -- Get file content
         dataStr <- mapM BS.readFile filePaths
+
+        -- Convert data
         let
+            -- json to internal representation
             artistData = map readArtist dataStr
-        putStrLn "Showing read data"
+            -- internal representation to xml module representation
+            xmlData = map makeXmlArtist artistData
+            -- xml module representation to xml string representation
+            xmlStr = map showElement xmlData
+
+        {- DEBUG
+        -- Print internal datatype
+        putStrLn " ========== Showing data ========== "
         print artistData
+        -}
 
-       --convert to xml and put in file
-        let xmlData = map makeXmlArtist artistData
-        putStrLn "Showing XML"
-        print xmlData
-        zipWithM writeToFile xmlData (map generateFilePath filePaths)
+        {- DEBUG
+        -- Print xml
+        putStrLn " ========== Showing XML ========== "
+        print xmlStr
+        -}
 
+        -- Write xml data to output files
+        zipWithM_ writeToFile xmlData (map generateFilePath filePaths)
+
+        -- Call example queries
         doQueries artistData
-        
-        putStrLn "End"
 
 -- Runs the json-to-XML from a directory of json files.
 -- This works, only if there aren't too many files in the dir.
 runFromDirectory :: FilePath -> IO ()
 runFromDirectory dir = do
+        -- Get json files on directory
+        fileNames <- getDirectoryContents dir
+        let filteredFileNames = filterJsonFile fileNames
+            fileDirs = replicate (length filteredFileNames) dir
+            filePaths = zipWith combine fileDirs filteredFileNames
+        -- Run on all files on directory
+        runFromFileList filePaths
 
-  fileNames <- getDirectoryContents dir
-  let filteredFileNames = filterJsonFile fileNames
-  let fileDirs = replicate (length filteredFileNames) dir
-  let filePaths = zipWith combine fileDirs filteredFileNames
-  runFromFileList filePaths
-        
-doQueries :: [Artist] -> IO ()
-doQueries artistData = do
-   -- Queries examples should be introduced here
-        putStrLn "Query 0: Primary artists alias"
-        print . maybeClean $ map getPrimAlias artistData
-        putStrLn "Query 1: Tags - Sorted by greater count first"
-        print . maybeClean $ map getTags artistData
-        putStrLn "Query 2: Artists that are from a specific country"
-        print . map artistName . filter (isFrom "US") $ artistData
-        putStrLn "Query 3: End date"
-        print . maybeClean $ map getEndDate artistData
+{--- Auxiliar functions ---}
 
-maybeClean :: Eq a => [Maybe a] -> [a]
-maybeClean = map fromJust . filter (/= Nothing)
+-- Parse a json string into an Artist
+readArtist :: BS.ByteString -> Artist
+readArtist = fromMaybe NullArtist . decode
 
--- Query 0:
--- Return artist primary alias if there is one or Nothing otherwise
-getPrimAlias :: Artist -> Maybe (String,String)
-getPrimAlias Artist{artistAliases = aliases, artistName = n} = primAlias aliases
-        where
-                primAlias :: [Alias] ->  Maybe (String, String)
-                primAlias [] = Nothing
-                primAlias ((Alias _ alias prim _ _):xs)
-                        | prim      = Just (n, alias)
-                        | otherwise = primAlias xs
-                primAlias _ = Nothing
-getArtistPrimAlias _ = Nothing
-
--- Query 1:
--- Return Tags list sorted by Tag count
-getTags :: Artist -> Maybe (String, [Tag])
-getTags Artist{ artistTags = []                   } = Nothing
-getTags Artist{ artistTags = tags, artistName = n } = Just (n, sortTags)
-        where
-                sortTags = sortBy (flip compare) tags
-getTags _ = Nothing
-
--- Query 2:
--- Returns whether an artist is from the country named on String
-isFrom :: String -> Artist -> Bool
-isFrom str Artist{artistCountry = country} = str == country
-isFrom   _                               _ = False
-
--- Query 3:
--- If the Artist ended its career, returns its ending date, otherwise, returns
--- nothing
-getEndDate :: Artist -> Maybe (String, String)
-getEndDate Artist{ artistLifeSpan = (LifeSpan _ endDate ended), artistName = n}
-        | ended  = Just (n, endDate)
-getEndDate _ = Nothing
-
--- Functions for writing xmls to file
-
+-- Write internal xml representation to file
 writeToFile :: Element -> FilePath -> IO ()
-writeToFile xml fileName = 
-  writeFile fileName $ showElement xml
+writeToFile xml fileName = writeFile fileName $ showElement xml
 
+-- Create the filepath for output xml file
 generateFilePath :: String -> FilePath
 generateFilePath jsonFileName =
-  replaceDirectory (replaceExtension jsonFileName "xml") "./xmls/"
+        replaceDirectory (replaceExtension jsonFileName "xml") "./xml/"
 
+-- Filter files
 filterJsonFile :: [String] -> [String]
-filterJsonFile files = filter notFile files
-  where notFile x = not (head x == '.')
+filterJsonFile = filter (\(x:_) -> x /= '.')
